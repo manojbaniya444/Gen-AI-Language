2c589b13565d43788c00269a110de961fc93ff65
manojbaniya444 <86000102+manojbaniya444@users.noreply.github.com>
2024-05-31 17:11:37 +0545

    Langchain course 1

---
+++ Gen AI Language/langchain/2] From Langchain course/Readme.md
@@ -0,0 +1,1 @@
+# This is Langchain from the course
---
+++ Gen AI Language/langchain/2] From Langchain course/lang_app.py
@@ -0,0 +1,26 @@
+from langchain import PromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+import os
+
+information = """Hi my name is john doe. I am 25 years old and I am a software engineer. I completed my Computer Science course from Stanford University and then i got internship at Google right after my university. Now I am working as a full time software engineer at Perplexity AI."""
+
+if __name__ == "__main__":
+    print("Hello from the langchain.")
+    
+    summary_template = """given the information about a person from I want to you to create:
+    
+    information: {information}
+
+    1. a short summary
+    2. two interesting facts about them
+    """
+    
+    summary_prompt_template = PromptTemplate(input_variables = ["information"], template = summary_template)
+    
+    # TODO: api key
+    llm = ChatOpenAI(temperature = 0, model_name="gpt-3.5-turbo")
+    
+    chain = LLMChain(llm = llm, prompt = summary_prompt_template)
+    
+    response = chain.run(information = information)
